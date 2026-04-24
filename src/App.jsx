007e3a923831193b136b9571c26966e52d28ba7e
JenniferW21 <jenniferwang570@gmail.com>
2026-04-24 00:12:27 +0000

    jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,121 +1,99 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+//import { useState } from 'react'
+//import reactLogo from './assets/react.svg'
+//import viteLogo from './assets/vite.svg'
+// import heroImg from './assets/hero.png'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const myStyle ={
+    backgroundColor: "lightblue", 
+    padding:"20px"
+}
 
+function Head(){
+  return (
+    <div className="container" style={myStyle}> 
+      <h1 className="heading">ABOUT ME</h1>
+    </div>
+  )
+}
+function Intro(props){
+  return (
+    <div className="container">
+      <h1 className="heading">Introduction</h1>
+      <p>Hi, my name is {props.name}!</p>
+    </div>
+  );
+}
+
+const Hobbies = ["Piano", "watching shows","Drawing",];
+
+function Interest(props){
+  return (
+    <div className="container">
+      <h1 className="heading">Interest & Hobbies</h1>
+      <ul>
+        <li> {props.hobbies[0]}</li>
+        <li> {props.hobbies[1]}</li>
+        <li> {props.hobbies[2]}</li>
+      </ul>
+    </div>
+  );
+}
+
+function Projects(){
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Hello Jennifer</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+    <div className="container">
+      <h1 className="heading" >Project</h1>
+    </div>
+    <div className="flex-container">
+      <span> <p>9th: Video Game</p> </span>
+      <span> <p>11th: Secret Decoder Ringer</p> </span>
+    </div>
     </>
+  );
+}
+
+function Footer(){
+  return (
+    <div className="container">
+      <p>Jennifer Wang | WPC Computer Programming | April 2026</p>
+    </div>
   )
 }
 
-export default App
+//Parent
+function Body(){
+  return (
+    <>
+    <div>
+      <Intro name="Jenn"/>
+      <Projects />
+      <Interest hobbies={Hobbies}/>
+    </div>
+    <div>
+      <Footer />
+    </div>
+    </>
+
+  );
+}
+
+
+
+
+
+
+function App() {
+  return (
+    <>
+      <Head/>
+      <Body /> 
+    </>
+  );
+}
+
+
+
+export default App;
+
